9c4c737e2141eb540248c76fe9498cb4084e7115
rajkarnikar11 <rajkarnikarsalil11@gmail.com>
2022-05-23 14:44:25 +0545

    burger hover added

--- src/js/main.js
+++ src/js/main.js
@@ -6,10 +6,25 @@ function toggleMenu() {
 		// document.querySelector(".nav__item").style.opacity =
 		// 	"1";
 		document.querySelector(".nav__item").style.display = "flex";
+		document.querySelector(".line__2").style.transform = "scale(0)";
+		document.querySelector(".line__1").style.transform =
+			"rotate(45deg) translate(-5px, -5px)";
+		document.querySelector(".line__1").style.transformOrigin = "top left";
+		document.querySelector(".line__3").style.transform = "rotate(-45deg)";
+		document.querySelector(".line__3").style.transformOrigin =
+			"bottom left";
+		document.querySelector(".line__2").style.transformOrigin = "center";
+		document.querySelector(".line__3").style.transitionDuration = ".3s";
+		document.querySelector(".line__2").style.transitionDuration = ".3s";
+		document.querySelector(".line__1").style.transitionDuration = ".3s";
 	} else {
 		toggler = 0;
 
 		document.querySelector(".nav__item").style.display = "none";
+		document.querySelector(".line__1").style.transform =
+			"rotate(0deg) translate(0px, 0px)";
+		document.querySelector(".line__2").style.transform = "scale(1)";
+		document.querySelector(".line__3").style.transform = "rotate(0deg)";
 	}
 }
 menu.addEventListener("click", toggleMenu);
