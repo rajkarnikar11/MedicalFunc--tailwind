0df91d5324b723bca407c96375e5b77817feabd2
rajkarnikar11 <rajkarnikarsalil11@gmail.com>
2022-05-24 10:03:37 +0545

    burger hiver fixed

--- src/js/main.js
+++ src/js/main.js
@@ -9,11 +9,12 @@ function toggleMenu() {
 		document.querySelector(".nav__item").style.display = "flex";
 		document.querySelector(".line__2").style.transform = "scale(0)";
 		document.querySelector(".line__1").style.transform =
-			"rotate(45deg) translate(-4px, -4px)";
-		document.querySelector(".line__1").style.transformOrigin = "top left";
-		document.querySelector(".line__3").style.transform = "rotate(-45deg)";
-		document.querySelector(".line__3").style.transformOrigin =
-			"bottom left";
+			"rotate(45deg) translateY(16px)";
+		// document.querySelector(".line__1").style.transformOrigin = "top left";
+		document.querySelector(".line__3").style.transform =
+			"rotate(-45deg ) translateY(-16px)";
+		// document.querySelector(".line__3").style.transformOrigin =
+		// 	"bottom left";
 		// document.querySelector(".line__2").style.transformOrigin = "center";
 		document.querySelector(".line__3").style.transitionDuration = ".3s";
 		document.querySelector(".line__2").style.transitionDuration = ".3s";
